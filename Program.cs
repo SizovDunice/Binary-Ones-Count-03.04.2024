1f1f645729da8ad1d4ab3f99c9363bb979561b84
Сизов Александр <a.sizov@sh.dunice.net>
2024-04-10 13:00:57 +0300

    create task

--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using BinaryCountClasses;
+
+BinaryCount.BinaryCountMethod(120);
+BinaryCount.BinaryCountMethod(10);
+BinaryCount.BinaryCountMethod(111);
